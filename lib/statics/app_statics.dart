3160395f594e22c353b0c0171291bb996ecd63e4
원하진 <wonhj12@naver.com>
2024-03-04 13:30:13 +0900

    feat: statistics update

--- lib/statics/app_statics.dart
+++ lib/statics/app_statics.dart
@@ -1,16 +1,30 @@
 import "package:flutter/widgets.dart";
 
 class AppStatics {
-  AppStatics._();
-
-  // Text colors
+  // Basic colors
   static const Color primary = Color(0xFF000000);
-  static const Color secondary = Color(0xFF808084);
+  static const Color secondary = Color(0xFF999999);
+  static const Color white = Color(0xFFFFFFFF);
 
   // App colors
+  // Brown
   static const Color brown = Color(0xFF734a3c);
+  static const Color brown400 = Color(0xFF8F6E63);
+  static const Color brown300 = Color(0xFFAB928A);
+  static const Color brown200 = Color(0xFFC7B7B1);
+  static const Color brown100 = Color(0xFFE3DBD8);
+  // LightBrown
   static const Color lightBrown = Color(0xFFD0B291);
+  static const Color lightBrown400 = Color(0xFFD9C1A7);
+  static const Color lightBrown300 = Color(0xFFE3D1BD);
+  static const Color lightBrown200 = Color(0xFFECE0D3);
+  static const Color lightBrown100 = Color(0xFFF6F0E9);
+  // Green
   static const Color green = Color(0xFFD1D68a);
+  static const Color green400 = Color(0xFFDADEA1);
+  static const Color green300 = Color(0xFFE3E6B9);
+  static const Color green200 = Color(0xFFEDEFD0);
+  static const Color green100 = Color(0xFFF6F7E8);
 
   // Font sizes
   static const double largeTitle = 34;
